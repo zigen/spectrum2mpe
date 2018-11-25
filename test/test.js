dfda0afa34652f6822b9ac5a5af6626de52c011f
zigen <hrlclb@gmail.com>
2018-11-25 19:29:14 +0900

    apply prettier

--- test/test.js
+++ test/test.js
@@ -8,8 +8,8 @@ s2m.convertFromSpear('large_bowl');
 
 // describe('Build s2m', function() {
 //   it(
-//       'should return type object', 
-//       function(){          
+//       'should return type object',
+//       function(){
 //         assert.typeOf(s2m.config,'object');
 //         assert.property(s2m.config, 'bpm');
 //       }
@@ -18,7 +18,7 @@ s2m.convertFromSpear('large_bowl');
 
 // describe('Convert from Spear', function() {
 //     it(
-//         'should return file type binary', 
+//         'should return file type binary',
 //         function(){
 //         s2m.convertFromSpear('large_bowl');
 //         }
@@ -27,7 +27,7 @@ s2m.convertFromSpear('large_bowl');
 
 // describe('Convert from Matlab', function() {
 //   it(
-//       'should return type object', 
+//       'should return type object',
 //       function(){
 //           s2m.build(environmentConfig);
 //           s2m.convertFromMatlab('bird');
